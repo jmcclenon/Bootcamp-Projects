9c4cfb031a6401a3c0d97f471cfb3fffc143faa2
JohnM <mars.john2018@gmail.com>
2023-05-10 14:32:01 -0400

    Bootcamp Week 9

---
+++ bootcamp-2022-week-9-twitter-clone-main/pages/index.js
@@ -0,0 +1,44 @@
+import { useSession } from 'next-auth/react'
+import { useRouter } from 'next/router'
+import prisma from 'lib/prisma'
+import { getTweets } from 'lib/data.js'
+import Tweets from 'components/Tweets'
+import Link from 'next/link'
+
+export default function Index({ tweets }) {
+  const { data: session, status } = useSession()
+  const router = useRouter()
+
+  if (status === 'loading') {
+    return null
+  }
+
+  if (session) {
+    router.push('/home')
+  }
+
+  return (
+    <div className='mt-10'>
+      <Tweets tweets={tweets} />
+      <div className='text-center p-4 border m-4'>
+        <h2 className='mb-10'>Join the conversation!</h2>
+        <Link href={'/api/auth/signin'}>
+          <a className='border px-8 py-2 mt-5 font-bold rounded-full color-accent-contrast bg-color-accent hover:bg-color-accent-hover-darker'>
+            login
+          </a>
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export async function getServerSideProps() {
+  let tweets = await getTweets(prisma, 3)
+  tweets = JSON.parse(JSON.stringify(tweets))
+
+  return {
+    props: {
+      tweets,
+    },
+  }
+}
